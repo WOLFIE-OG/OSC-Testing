1017a021452c2ed2588b588953d8cd45f36b5f1e
WOLFIE_OG <oliverjtodd@yahoo.com>
2022-02-24 14:50:20 +0000

    quick fix

--- libs/osc_server.py
+++ libs/osc_server.py
@@ -1,24 +1,27 @@
 from pythonosc.osc_server import AsyncIOOSCUDPServer, Dispatcher
 import asyncio
 
-def filter_handler(address, *args):
+def vrc_emote_handler(address, *args):
     print(f"{address}: {args}")
-    
+
 class OSCServer:
 
     def __init__(self):
+        self.is_open = False
         self.dispatcher = Dispatcher()
-        self.dispatcher.map("/avatar/parameters/VRCEmote", filter_handler)
+        self.dispatcher.map("/avatar/parameters/VRCEmote", vrc_emote_handler)
     
     async def loop(self):
-        while True:
-            print(f"Test")
+        while self.is_open is not False:
+            print(f"Open")
             await asyncio.sleep(1)
+        self.stop()
 
     async def start(self):
         srv = AsyncIOOSCUDPServer(("127.0.0.1", 9001), self.dispatcher, asyncio.get_event_loop())
-        transpt, prtc = await srv.create_serve_endpoint()
-        
+        self.transpt, prtc = await srv.create_serve_endpoint()
+        self.is_open = True
         await self.loop()
-
-        transpt.close()
+    
+    async def stop(self):
+        self.transpt.close()
